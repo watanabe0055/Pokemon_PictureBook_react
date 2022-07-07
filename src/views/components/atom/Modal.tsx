6bba62300482cacbfa0ef642991683106eccbb5c
watanabe0055 <mailok1212+github@gmail.com>
2022-07-08 00:25:31 +0900

    カードからモーダルにpropsを渡すところまで実装

--- src/views/components/atom/Modal.tsx
+++ src/views/components/atom/Modal.tsx
@@ -1,13 +1,85 @@
 import React from "react";
 import styled from "styled-components";
 
-const PokemonModal = styled.div`
-  border: 1px solid red;
+const PokemonCard = styled.div`
+  width: 200px;
+  height: 180px;
+  margin: 15px;
+  border-radius: 10px;
+  box-shadow: 0 2px 5px #ccc;
+  background-color: #f9f9f9;
 `;
-const Modal = () => {
+
+const CardItems = styled.div`
+  padding: 3px;
+`;
+const Img = styled.img`
+  height: 100px;
+  width: 100px;
+`;
+
+const InfoValue = styled.div`
+  margin-left: 25px;
+`;
+
+const PokemonImgBox = styled.div`
+  text-align: center;
+`;
+const Modal: any = (props: any) => {
+  let { image, id, name, type, att, def, sp_att, sp_def, speed } = props;
+
+  console.log(id);
+  console.log(name);
   return (
     <>
-      <PokemonModal>modalです</PokemonModal>
+      {/* <PokemonCard>
+        <CardItems>
+          <PokemonImgBox>
+            <Img
+              src={
+                image ||
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+              }
+              alt={`${name}の画像`}
+            />
+          </PokemonImgBox>
+        </CardItems>
+        <CardItems>
+          <InfoValue>No:{id || "001"}</InfoValue>
+        </CardItems>
+        <CardItems>
+          <InfoValue>{name || "フシギダネ"}</InfoValue>
+        </CardItems>
+        <CardItems>
+          <p>タイプ:</p>
+          <InfoValue>{type}</InfoValue>
+        </CardItems>
+
+        <CardItems>
+          <p>攻撃：</p>
+          <InfoValue>{att}</InfoValue>
+        </CardItems>
+
+        <CardItems>
+          <p>防御：</p>
+          <InfoValue>{def}</InfoValue>
+        </CardItems>
+
+        <CardItems>
+          <p>特攻：</p>
+          <InfoValue>{sp_att}</InfoValue>
+        </CardItems>
+
+        <CardItems>
+          <p>特防：</p>
+          <InfoValue>{sp_def}</InfoValue>
+        </CardItems>
+
+        <CardItems>
+          <p>素早さ:</p>
+          <InfoValue>{speed}</InfoValue>
+        </CardItems>
+      </PokemonCard> */}
     </>
   );
 };
