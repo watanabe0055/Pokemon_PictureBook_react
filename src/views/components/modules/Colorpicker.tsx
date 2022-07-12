c1cea5f66bc0d15a7dd8ae44684888489a04fa39
watanabe0055 <mailok1212+github@gmail.com>
2022-07-13 01:02:00 +0900

    colorpickerのlocalStorageni

--- src/views/components/modules/Colorpicker.tsx
+++ src/views/components/modules/Colorpicker.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Colorpicker = (props: any) => {
   const colorPicker = (color: string) => {
     props.setColort(color);
+    localStorage.setItem("pickColor", color);
   };
+
   return (
     <input
       type="color"
